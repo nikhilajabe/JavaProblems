17cb1ba0e6eb0da47ec2d360e86916d859ecdd5e
nikhilajabe <140068919+nikhilajabe@users.noreply.github.com>
2023-11-28 16:05:23 +0530

    Update DayoftheWeek.java

--- DayoftheWeek.java
+++ DayoftheWeek.java
@@ -7,12 +7,12 @@ class Solution {
         int[] m = {0, 31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31};      
         int d = day;
 
-        // Add the days of the months before the current month to 'd'
+        // Add the days of the months before the current month to d
         for (int i = 1; i < month; i++) {
             d += m[i];
         }
 
-        // Check for a leap year and increment 'd' if the month is after February
+        // Check for a leap year and increment d if the month is after February
         if (isLeapYear(year) && month > 2) {
             d++;
         }
@@ -21,7 +21,7 @@ class Solution {
         year--;
         d += (year + (year / 4) - (year / 100) + (year / 400)) % 7;
 
-        // Return the corresponding day of the week from the 's' array
+        // Return the corresponding day of the week from the s array
         return s[(d) % 7];
     }
 
