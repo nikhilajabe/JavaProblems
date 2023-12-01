a3e0b72676becdbfba28f7d85e4ace6a91cca450
nikhilajabe <140068919+nikhilajabe@users.noreply.github.com>
2023-12-01 14:47:45 +0530

    Update Convert1DarrayInto2D.java

--- Convert1DarrayInto2D.java
+++ Convert1DarrayInto2D.java
@@ -1,13 +1,13 @@
 class Convert1DarrayInto2D {
     public int[][] construct2DArray(int[] original, int m, int n) {
         int arr2D[][]=new int[m][n];
-        if(m*n!=original.length){
-            return new int [0][0];
+        if(m*n!=original.length){  //
+            return new int [0][0]; //return empty array if not fit
         }
 
-        for(int i=0;i<m;i++){
+        for(int i=0;i<m;i++){    //iteration over array
             for(int j=0;j<n;j++){
-                arr2D[i][j]=original[i*n+j];
+                arr2D[i][j]=original[i*n+j];  //add element  in 2Darray
             }
         }
         return arr2D;
